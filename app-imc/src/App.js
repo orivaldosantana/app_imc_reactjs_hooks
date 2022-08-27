f440102be117f88c075cd6a452549a1ef4621db8
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-08-27 12:13:33 -0300

    form e css

--- app-imc/src/App.js
+++ app-imc/src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
+import Form from './components/form'
 import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
       <Header title="Cálculo do IMC" />
+      <div className="Body">
+        <Form />
+      </div>
     </div>
   )
 }
---
+++ app-imc/src/components/form.js
@@ -0,0 +1,14 @@
+const Form = () => {
+  return (
+    <div className="Form">
+      <form>
+        <label>Massa:</label>
+        <input type="number" required value="0" />
+        <label>Altura:</label>
+        <input type="number" required value="0" />
+      </form>
+    </div>
+  )
+}
+
+export default Form
