8f3649cdef6b68ee12d5e9f0ee0aa0fd99889401
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-09-01 22:55:31 -0300

    capturando dados de formulário com useSate

--- app-imc/src/components/form.js
+++ app-imc/src/components/form.js
@@ -1,11 +1,30 @@
+import { useState } from 'react'
+
 const Form = () => {
+  const [mass, setMass] = useState('')
+  const [height, setHeight] = useState('')
   return (
     <div className="Form">
       <form>
         <label>Massa: </label>
-        <input type="number" required value="0" />
+        <input
+          type="number"
+          required
+          value={mass}
+          onChange={e => {
+            console.log(e.target.value)
+            setMass(e.target.value)
+          }}
+        />
         <label>Altura: </label>
-        <input type="number" required value="0" />
+        <input
+          type="number"
+          required
+          value={height}
+          onChange={e => {
+            setHeight(e.target.value)
+          }}
+        />
       </form>
     </div>
   )
